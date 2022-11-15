f6cf4f2bc9a041cd57cba052112747ad9099dd41
LimitQueenProject <jayabayatools@gmail.com>
2022-11-15 23:47:00 +0700

    awokwokwokw

--- 2player.c
+++ 2player.c
@@ -17,16 +17,21 @@ void markPlayer2();
 void clear();
 
 
+int cekVertikal();
+int cekPemenang();
+
 int main()
 {
+	int pemenang = 0;
+	
 	gameBoard();
 	inputNamaPlayer();
 	
 	system("cls");
 	
 	
-	while ( giliran <= 9 )
-	{
+	
+	do {
 		if ( giliran % 2 == 1 )
 		{
 			gameBoard();
@@ -47,12 +52,123 @@ int main()
 			clear();
 			gameBoard();
 		}	
+		
+		pemenang = cekPemenang();
+		
+		
 	
+	} while ( giliran <= 9 && pemenang == 0 );
+	
+	
+	if ( pemenang == 1 )
+	{
+		printf("Selamat player 1 menang aokwokwkko kontol\n");
+	}
+	else if ( pemenang == 2 )
+	{
+		printf("Selamat player 2 menang aokwokwkko MEMEK\n");
+	}
+	else if ( pemenang == 3 )
+	{
+		printf("DRAWWWW!!!!!\n");
+		
 	}
 	
-	
+		
 	return 0;
 }
+
+int cekPemenang()
+{
+	
+	if ( 
+	papan[1] != '1' && 
+	papan[2] != '2' && 
+	papan[3] != '3' && 
+	papan[4] != '4' && 
+	papan[5] != '5' && 
+	papan[6] != '6' && 
+	papan[7] != '7' && 
+	papan[8] != '8' && 
+	papan[9] != '9' )
+	{
+		return 3;
+	}
+	else if ( papan[1] == 'X' && papan[2] == 'X' && papan[3] == 'X' )
+	{
+		return 1;
+	}
+	else if ( papan[4] == 'X' && papan[5] == 'X' && papan[6] == 'X' )
+	{
+		return 1;
+	}
+	else if ( papan[7] == 'X' && papan[8] == 'X' && papan[9] == 'X' )
+	{
+		return 1;
+	}
+	else if ( papan[1] == 'X' && papan[4] == 'X' && papan[7] == 'X' )
+	{
+		return 1;
+	}
+	else if ( papan[2] == 'X' && papan[5] == 'X' && papan[8] == 'X' )
+	{
+		return 1;
+	}
+	else if ( papan[3] == 'X' && papan[6] == 'X' && papan[9] == 'X' )
+	{
+		return 1;
+	}
+	else if ( papan[1] == 'X' && papan[5] == 'X' && papan[9] == 'X' )
+	{
+		return 1;
+	}
+	else if ( papan[3] == 'X' && papan[5] == 'X' && papan[7] == 'X' )
+	{
+		return 1;
+	}
+	
+	// O
+	else if ( papan[1] == 'O' && papan[2] == 'O' && papan[3] == 'O' )
+	{
+		return 2;
+	}
+	else if ( papan[4] == 'O' && papan[5] == 'O' && papan[6] == 'O' )
+	{
+		return 2;
+	}
+	else if ( papan[7] == 'O' && papan[8] == 'O' && papan[9] == 'O' )
+	{
+		return 2;
+	}
+	else if ( papan[1] == 'O' && papan[4] == 'O' && papan[7] == 'O' )
+	{
+		return 2;
+	}
+	else if ( papan[2] == 'O' && papan[5] == 'O' && papan[8] == 'O' )
+	{
+		return 2;
+	}
+	else if ( papan[3] == 'O' && papan[6] == 'O' && papan[9] == 'O' )
+	{
+		return 2;
+	}
+	else if ( papan[1] == 'O' && papan[5] == 'O' && papan[9] == 'O' )
+	{
+		return 2;
+	}
+	else if ( papan[3] == 'O' && papan[5] == 'O' && papan[7] == 'O' )
+	{
+		return 2;
+	}
+
+	else
+	{
+		return 0;
+	}
+}
+
+
+
 
 void markPlayer1()
 {
@@ -98,6 +214,9 @@ void markPlayer2()
 }
 
 
+
+
+
 void inputNamaPlayer()
 {
 	printf("Nama player 1 : ");
@@ -119,6 +238,7 @@ void gameBoard()
     printf("  %c |  %c |  %c \n", papan[7], papan[8], papan[9]);
     printf("\n\n\n");
 }
+
 
 void clear()
 {
