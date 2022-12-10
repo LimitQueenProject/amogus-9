796c2db68a5c00e11f010e30c43a162531c03c1b
LimitQueenProject <jayabayatools@gmail.com>
2022-12-10 13:50:56 +0700

    hh

---
+++ Amogus_Upload/ai.c
@@ -0,0 +1,25 @@
+// #include <stdio.h>
+// #include <stdbool.h>
+
+
+
+// void medium( int *_baris, int *_kolom, char _papan[7][7] )
+// {
+//     typedef struct
+//     {
+//         int baris;
+//         int kolom;
+//         bool indexValid;
+//     }Index;
+
+//     Index index[9];
+
+//     index[0].baris = *_baris;
+//     index[0].kolom = *_kolom;
+
+//     if( index[0].baris - 1 >= 0 && index[0].kolom - 1 >= 0 )
+//     {
+
+//     }
+    
+// }
